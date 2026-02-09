c536348566cbbbd512e3f2b897800fb5606baed4
Dmitry Dyachkov <git@e965.ru>
2026-02-09 21:46:34 +0500

    chore: fixes

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,12 @@ import "dotenv/config";
 import { env, type PrismaConfig } from "prisma/config";
 
 export default {
-  schema: "db/schema.prisma",
+  schema: "./db/schema.prisma",
   datasource: {
     url: env("DATABASE_URL"),
   },
   migrations: {
-    path: "db/migrations",
-    seed: "node --no-warnings --loader ts-node/esm --experimental-specifier-resolution=node db/seed.ts",
+    path: "./db/migrations",
+    seed: "node --no-warnings --loader ts-node/esm --experimental-specifier-resolution=node ./db/seed.ts",
   },
 } satisfies PrismaConfig;
