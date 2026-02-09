137021e073ad40a8cc7d7b08246d93410fc078c7
Dmitry Dyachkov <git@e965.ru>
2026-02-09 21:52:11 +0500

    chore: fixes

--- db/generators/dmmf.ts
+++ db/generators/dmmf.ts
@@ -1,5 +1,5 @@
-import { mkdir, writeFile } from "node:fs/promises";
-import { join } from "node:path";
+import fs from "node:fs/promises";
+import path from "node:path";
 
 import generatorHandler from "@prisma/generator-helper";
 
@@ -13,11 +13,11 @@ generatorHandler.generatorHandler({
     const outputFileName = options.generator.output?.value;
     if (!outputFileName) throw new Error("No output file defined for DMMF generator");
 
-    const outputDir = join(outputFileName, "..");
-    await mkdir(outputDir, { recursive: true });
+    const outputDir = path.join(outputFileName, "..");
+    await fs.mkdir(outputDir, { recursive: true });
 
     const content = `export const dmmf = ${JSON.stringify({ datamodel: { models: options.dmmf.datamodel.models } })} as const;`;
 
-    await writeFile(outputFileName, content);
+    await fs.writeFile(outputFileName, content);
   },
 });
